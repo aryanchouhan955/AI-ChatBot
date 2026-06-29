2679b5550696b84d8e578e3df133e5cead68f2ed
Aryan Chouhan <aryanchouhan955@gmail.com>
2026-06-29 11:21:37 +0530

    Changed Model

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
       history.push({ role: "user", parts: [{ text: user.text }] });
 
       const res = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-3.5-flash",
         contents: history,
       });
       setTypingMessageId(aiMsg.id);
